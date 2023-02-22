45e311f2917342d7be281f36cd3fd1ac3d65e8f8
Sebastian Świeczkowski <sebastianswiecz458@gmail.com>
2023-02-22 13:15:25 +0100

    Initial config

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <main>
+      <div className='min-h-screen flex justify-center items-center'>
+        <h1 className='text-3xl font-bold text-blue-600'>Shopping queue</h1>
+      </div>
+    </main>
+  );
+}
+
+export default App;
